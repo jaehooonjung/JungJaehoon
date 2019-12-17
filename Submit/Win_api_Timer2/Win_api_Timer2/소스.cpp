8992b5f64db4865a09f0ca0927e8f7640e229ec5
jaehooonjung <58425812+jaehooonjung@users.noreply.github.com>
2019-12-17 17:39:00 +0900

    2
    2

--- Submit/Win_api_Timer2/Win_api_Timer2/소스.cpp
+++ Submit/Win_api_Timer2/Win_api_Timer2/소스.cpp
@@ -1,9 +1,14 @@
 #include<windows.h>
+#include<math.h>
+#define PI 3.14
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = TEXT("Clock");
-
+SYSTEMTIME st;
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime);
+void ClockDraw(int degree, int length, float &i, float &j);
+float DegreeToRadian(float degree);
+
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmdParam, int nCmdShow)
 {
@@ -36,27 +41,31 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmd
 }
 
 int x = 200;
-int y = 400;
-int SecondX;
-int SecondY;
-
+int y = 200;
+float second_x, second_y;
+float minute_x, minute_y;
+float hour_x, hour_y;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
-	SYSTEMTIME st;
 	static TCHAR sTime[128];
-
 	switch (iMessage)
 	{
 	case WM_CREATE:
 		SetTimer(hWnd, 1, 100, TimeProc);
 		SendMessage(hWnd, WM_TIMER, 1, 0);
 		return 0;
+		return 0;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
-		Ellipse(hdc, x, y, x + 50, y + 50);
+		MoveToEx(hdc, x, y, NULL);
+		LineTo(hdc, second_x, second_y);
+		MoveToEx(hdc, x, y, NULL);
+		LineTo(hdc, minute_x, minute_y);
+		MoveToEx(hdc, x, y, NULL);
+		LineTo(hdc, hour_x, hour_y);
 		EndPaint(hWnd, &ps);
 		return 0;
 	case WM_DESTROY:
@@ -69,11 +78,56 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 
 void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
 {
-	//선의 길이 변경
+	GetLocalTime(&st);
+	ClockDraw(st.wSecond, 80, second_x, second_y);
+	ClockDraw(st.wMinute, 50, minute_x, minute_y);
+	ClockDraw(st.wHour, 30, hour_x, hour_y);
 	InvalidateRect(hWnd, NULL, TRUE);
 }
 
-void SecondDraw()
+void ClockDraw(int degree, int length, float &i, float &j)
 {
+	float a, b;
+	float Radian = DegreeToRadian((float)degree*6);
+	b = sin(Radian) * length;
+	a = cos(Radian) * length;
+
+	i = x + a;
+	j = y + b;
+
+
+
+	/*
+		if (degree >= 15 && degree < 30)
+	{
+		i = x - a;
+		j = y + b;
+	}
+	else if (degree >= 30 && degree < 45)
+	{
+		i = x - a;
+		j = y - b;
+
+	}
+	else if (degree >= 45 && degree <  60)
+	{
+		i = x + a;
+		j = y - b;
+	}
+	else
+	{
+		i = x + a;
+		j = y + b;
+
+	}
+
+	*/
 
 }
+
+
+float DegreeToRadian(float degree)
+{
+	return degree*PI/180;
+}
+
