17b17772e6806e52da4b01d59898cd3b9fb057a0
jaehooonjung <58425812+jaehooonjung@users.noreply.github.com>
2020-02-13 19:01:34 +0900

    getkeystate
    getkeystate update

---
+++ Submit/previous/Win_api_ChessGameRemaster/Win_api_ChessGameRemaster/BlockManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Block.h"
+
+class BlockManager
+{
+private:
+	vector<Block*> BlockList;
+	Block*m_MarkingBlock;
+public:
+	void BlockInit(HDC hdc);
+	void BlockDelete();
+	void BlockDraw(HDC hdc);
+	void MarkingBlockInit(HDC hdc);
+	void MarkingBlockDraw(HDC hdc, POINT pos);
+	BlockManager();
+	~BlockManager();
+};
